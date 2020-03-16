171ee23ce855d78f29d2cbd0678b74b72dacaaa2
nrusso21 <nrusso@protonmail.com>
2020-03-16 12:58:56 -0400

    Fix build pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,14 @@
 module.exports = {
-    plugins: [
-        require("postcss-easy-import"),
-        require("tailwindcss"),
-        process.env.NODE_ENV === "production"
-            ? [
-                "@fullhuman/postcss-purgecss",
-                {
-                    content: [
-                        "./pages/**/*.{js,jsx,ts,tsx}",
-                        "./components/**/*.{js,jsx,ts,tsx}"
-                    ],
-                    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-                }
-            ]
-            : undefined,
-        require("autoprefixer"),
-        require("cssnano")
-    ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: ["./components/**/*.js", "./pages/**/*.js"],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        }
+      : {})
+  }
 };
